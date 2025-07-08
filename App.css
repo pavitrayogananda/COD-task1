5bace0e328d72e97a627c0048c0bd9ef5cda155a
pavitrayogananda <pavitrayogananda@gmail.com>
2025-07-08 10:45:39 +0530

    Add files via upload

---
+++ App.css
@@ -0,0 +1,35 @@
+.app {
+  text-align: center;
+  margin-top: 50px;
+  font-family: Arial, sans-serif;
+}
+
+input {
+  padding: 10px;
+  width: 200px;
+  margin-right: 10px;
+  font-size: 16px;
+}
+
+button {
+  padding: 10px 15px;
+  font-size: 16px;
+  background-color: #3498db;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.weather-info {
+  margin-top: 20px;
+  background: #f1f1f1;
+  display: inline-block;
+  padding: 20px;
+  border-radius: 10px;
+}
+
+.error {
+  color: red;
+  margin-top: 15px;
+}
